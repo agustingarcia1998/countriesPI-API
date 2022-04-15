1d0300c511eb5828b75121929663185c491aff37
Agustin Garcia <94334480+agustingarcia1998@users.noreply.github.com>
2022-04-14 21:04:23 -0300

    Force

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 ///////CAMBIO
 const PORT = process.env.PORT || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
